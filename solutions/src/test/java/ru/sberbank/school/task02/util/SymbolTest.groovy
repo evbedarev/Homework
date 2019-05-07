36497f96fc80ac82b16c282286ea09261c5c8ea5
Alex K <bormoshka@gmail.com>
2019-05-07 20:47:45 +0300

    # Conflicts: #	Jenkinsfile.groovy #	solutions/src/main/java/ru/sberbank/school/task02/services/InternalQuotesService.java #	solutions/src/main/java/ru/sberbank/school/task02/services/implementation/ExternalQuotesServiceImpl.java #	solutions/src/main/java/ru/sberbank/school/task02/services/implementation/FxConversionServiceImpl.java #	solutions/src/main/java/ru/sberbank/school/task02/services/implementation/InternalQuotesServiceImpl.java #	solutions/src/main/java/ru/sberbank/school/task02/services/implementation/ServiceFactoryImpl.java #	solutions/src/test/java/ru/sberbank/school/task02/util/QuoteBuilderTest.java #	solutions/src/test/java/ru/sberbank/school/task02/util/SymbolTest.groovy

--- solutions/src/test/java/ru/sberbank/school/task02/util/SymbolTest.groovy
+++ solutions/src/test/java/ru/sberbank/school/task02/util/SymbolTest.groovy
@@ -5,22 +5,22 @@ import static ru.sberbank.school.task02.util.Symbol.*
 class SymbolTest extends GroovyTestCase {
 
     void testGetSymbol1() {
-        String target = "USD/RUB"
-        String value = USD_RUB.getSymbol()
-        assertEquals(target, value)
+        String target = "USD/RUB";
+        String value = USD_RUB.getSymbol();
+        assertEquals(target, value);
     }
 
     void testGetSymbol2() {
-        String target = "RUB/USD"
-        String value = RUB_USD.getSymbol()
-        assertEquals(target, value)
+        String target = "RUB/USD";
+        String value = RUB_USD.getSymbol();
+        assertEquals(target, value);
     }
 
     void testIsCross1() {
-        assertEquals(false, USD_RUB.isCross())
+        assertEquals(false, USD_RUB.isCross());
     }
 
     void testIsCross2() {
-        assertEquals(true, RUB_USD.isCross())
+        assertEquals(true, RUB_USD.isCross());
     }
 }
